5c205ecd52adbc6b2658cc894d2ce1cec281afa8
Ciaran Martin Conners <ciaranconners@gmail.com>
2017-05-15 18:24:56 -0400

    Done With GetElements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,41 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  // use document.element to get the classList of an element
+  // check if the classlist has the className and matches with the passed className
+  // if yes, store it in an array(nodelist)
+  // move on to check, if the element has any children,
+  // if yes, run the function again on it to get the elements with the passed className
+
+  var result = [];
+
+  var getElements = function(element) {
+    if (element.classList) {
+      if (element.classList.contains(className)) {
+        result.push(element);
+      }
+    }
+    for (var i = 0; i < element.childNodes.length; i++) {
+      getElements(element.childNodes[i]);
+    }
+  };
+  getElements(document.body);
+  return result;
 };
+
+// ver 2
+// var getElementsByClassName = function(className, node) {
+//   var result = [];
+//   node = node || document.body;
+//   if (node.classList) {
+//     if (node.classList.contains(className)) {
+//       result.push(node);
+//     }
+//   }
+//   for (var i = 0; i < node.childNodes.length; i++) {
+//     result = result.concat(getElementsByClassName(className, node.childNodes[i]));
+//   }
+//   return result;
+
+// };
